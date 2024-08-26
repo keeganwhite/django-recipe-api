76d35f830f4ac1d048bf8090111f18028567229c
keegan <keeganthomaswhite@gmail.com>
2024-08-26 12:40:27 +0100

    fixed flake8 errors

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,9 @@
 Database models
 """
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
+
 
 class UserManager(BaseUserManager):
     """Manager for users"""
@@ -13,6 +15,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Test models command
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test case for models"""
     def test_create_user_with_email_successful(self):
