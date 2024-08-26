f6c09ea6d66416762036690d435c681a70760763
keegan <keeganthomaswhite@gmail.com>
2024-08-26 12:54:25 +0100

    added superuser and test cases, plus email normalization test cases

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,3 +17,29 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['TEST4@EXAMPLE.COM', 'TEST4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'pass')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating a new user with no email is unsuccessful"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'pass')
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='test'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
